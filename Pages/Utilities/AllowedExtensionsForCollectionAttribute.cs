716d57d5cc0bd1dc0000525b117bab481d151466
Rohit Harlapur <48487317+RohitRH@users.noreply.github.com>
2020-06-09 18:56:05 +0530

    Add files via upload

---
+++ Pages/Utilities/AllowedExtensionsForCollectionAttribute.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FirstApi.Utilities
+{
+    public class AllowedExtensionsForCollectionAttribute : ValidationAttribute
+    {
+        private readonly string[] _extensions;
+        public AllowedExtensionsForCollectionAttribute(string[] extensions)
+        {
+            _extensions = extensions;
+        }
+
+        protected override ValidationResult IsValid(object value, ValidationContext validationContext)
+        {
+            var files = value as IFormFileCollection;
+            if (files == null)
+            {
+                return new ValidationResult("Please select files");
+            }
+            
+            foreach (var file in files)
+            {
+                
+                if (file != null)
+                {
+                    var extension = Path.GetExtension(file.FileName);
+                    if (!_extensions.Contains(extension.ToLower()))
+                    {
+                        return new ValidationResult("Allowed Extensions are pdf,doc,docx Inavalid for " + file.FileName);
+                    }
+                }
+            }
+            return ValidationResult.Success;
+        }
+    }
+}
---
+++ Pages/Utilities/MaxSizeAttribute.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FirstApi.Utilities
+{
+    public class MaxSizeAttribute : ValidationAttribute
+    {
+        private int maxSize;
+        public MaxSizeAttribute(int _maxSize)
+        {
+            maxSize = _maxSize;
+        }
+
+        protected override ValidationResult IsValid(object value, ValidationContext validationContext)
+        {
+            var file = value as IFormFile;
+            if (file != null)
+            {
+                if (file.Length > maxSize)
+                {
+                    return new ValidationResult("Max file size allowed is 5Mb "+file.FileName+" file size is "+ file.Length/1024/1025 + " Mb");
+                }
+
+                if (file.Length == 0)
+                {
+                    return new ValidationResult("File Size Cannot be Zero "+file.FileName);
+                }
+            }
+
+            return ValidationResult.Success;
+        }
+    }
+}
